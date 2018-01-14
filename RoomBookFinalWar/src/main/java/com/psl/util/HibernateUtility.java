c13142c4eeb23d00b6ddca3bdc526860ec1c3f4e
Robertron <vishalsnedan@gmail.com>
2018-01-14 18:19:38 +0530

    Tomcat Working Project With Hibernate Core

--- RoomBookFinalWar/src/main/java/com/psl/util/HibernateUtility.java
+++ RoomBookFinalWar/src/main/java/com/psl/util/HibernateUtility.java
@@ -6,11 +6,11 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtility {
 	private static SessionFactory sf;
-	static {
-		sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-	}
 
 	public static Session getSesssion() {
+
+		sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
 		Session session = sf.openSession();
 		return session;
 	}
